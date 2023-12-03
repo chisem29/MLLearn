bb80194f407958a123e28c04dfb3b482504bb547
samChisem29 <tonyfhg1000@gmail.com>
2023-12-03 21:24:51 +0300

    sd

--- Python/Pandas.py
+++ Python/Pandas.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 #создание списка типа int[]
-arr = pd.array([1, 2, 3, 2, 5, "2", None, True, False, 1, 0])
-print(pd.DataFrame(arr.unique()))
 
+dt = pd.DataFrame({"A" : [1, 2, 3], "B" : [1, 2, 3]}, index=["row1", "row2"])
+
+print(dt[dt["A"]>2])
 #DArrow
 
 #TimeStamp
-
+ 
 #с помощью timestamp мы можем в опр. формате получить заданную дату
 
 print(pd.Timestamp(year=2023, month=11, day=29, hour=20, minute=52).timetuple()) 
