314926ab49bfe291ad5225c9d195ed09f2d9cba0
samChisem29 <tonyfhg1000@gmail.com>
2024-05-10 20:10:58 +0300

    fixx

---
+++ Python/Descriptors.py
@@ -0,0 +1,75 @@
+from typing import Any
+from dataclasses import dataclass, field
+
+class TypingText :
+
+    """def __init__(self, attr_name) : НЕ ИСПОЛЬЗОВАТЬ
+        self.attr_name = f'_{attr_name}'
+        self.l = [] """
+    
+    def __set_name__(self, owner, name : str) -> None : # WELL WELL WELL
+        self.attr_name = f'_{name}'
+        self.l = []
+        print(name, owner)
+
+    def __get__(self, intance, owner=None) -> Any : # срабатывает при вызове класса
+        return getattr(intance, self.attr_name)
+    
+    def __set__(self, intance, value : Any) -> None : # срабатывает при изменении 
+        self.l.append(value)
+        setattr(intance, self.attr_name, self.l)
+        
+    def __delete__(self, intance) -> None : # срабатывает при удалении класса
+        self.l = []
+        delattr(intance, self.attr_name)
+
+class MyTypingText :
+    x = TypingText()
+
+cl = MyTypingText()
+text = 'I ll catch the world'
+for c in text :
+    cl.x = c
+print(cl.x)
+del cl.x
+
+class Obj :
+    __slots__ = ['name', 'age', '_surname']
+    def __init__(self) -> None:
+        self._surname = None
+    @property
+    def surname(self) :
+        return self._surname
+    @surname.setter
+    def surname(self, value) :
+        self._surname = f'{value} Jopa'
+    @surname.getter
+    def surname(self) :
+        return self._surname
+
+person = Obj()
+person.name = 'Jhon'
+person.age = 23
+person.surname = 'Banditov'
+
+print(person.surname)
+
+try :
+    person.loc = 'Mexixco'
+except AttributeError :
+    print('Нельзя создать т.к заданы ограничения у атрибутов с помощью __slots__!!!!')
+
+@dataclass
+class Actor :
+    name:str
+    age:int
+    country:str=None
+
+@dataclass(frozen=True)
+class Film :
+    title:str
+    actors:list[Actor]
+    year:int
+
+opengaymer = Film(title='Opengaymer', actors=[Actor(name=name, age=age, country=country) for name, age, country in [["Bob", 21, 'US'], ["Pidor", 43, 'US'], ['Jack', 19, 'US']]], year=2023)
+print(opengaymer.actors)
--- Python/PandasNumpy.py
+++ Python/PandasNumpy.py
@@ -1,21 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from numpy import random
+from matplotlib.ticker import MultipleLocator, FixedFormatter, IndexLocator
 
-plt.subplot(2, 2, 1)
-plt.bar(list('abcdefghjk'.upper()), [random.randint(10, 50) * 1000 for i in range(10)], color="orange")
-plt.title("Salary")
+## https://habr.com/ru/articles/468295/
+## WRITE SOLUTIONS 15 TASKS!!! 
+df = pd.DataFrame([[pd.Timestamp('10-03-16') ,774.25,776.065002,769.5,772.559998], 
+                   [pd.Timestamp('10-04-16'), 776.030029,778.710022,772.890015,776.429993], 
+                   [pd.Timestamp('10-05-16'), 779.309998,782.070007,775.650024,776.469971], 
+                   [pd.Timestamp('10-06-16'), 779,780.47998,775.539978,776.859985], 
+                   [pd.Timestamp('10-07-16'), 779.659973,779.659973,770.75,775.080017]], 
+                   columns=['Date','Open','High','Low','Close'])
 
-plt.subplot(2, 2, 2)
-plt.bar(list('abcdefghjk'.upper()), [random.randint(1, 10) * 10 for i in range(10)], color="green")
-plt.title("RateLike")
+df['Date'] = df['Date'].dt.day
+d = df.plot(linewidth=2, marker='o', figsize=[7, 4])
+d.grid(which='major')
 
-plt.subplot(2, 2, 3)
-plt.pie([35, 25, 25, 15], labels = ["Apples", "Bananas", "Cherries", "Dates"], explode=[0, 0, 0, 0.2])
+d.yaxis.set_major_locator(MultipleLocator(10))
+d.xaxis.set_major_locator(IndexLocator(1, 0))
+d.xaxis.set_major_formatter(FixedFormatter(df['Date']))
+
+d.set(ylim=(750, 800))
+
+d.set_xlabel('Days', fontdict={"size":12})
+d.set_ylabel('Income', fontdict={"size":12})
+
+d.legend(['Open','High','Low','Close'])
+
 plt.show() 
 
-plt.suptitle("TOP-10 SALARIES")
-
-plt.show()
-
-# Дальше только хуже
